ab6f74862af4def30fd5f327f37a80ace279f5e8
Danielv210796 <erik.vargas18@tectijuana.edu.mx>
2020-11-13 13:56:18 -0600

    Termiado

---
+++ PracticaSemanal1/Controllers/ClasicasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PracticaSemanal1.Controllers
+{
+    public class ClasicasController : Controller
+    {
+        // GET: Clasicas
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PracticaSemanal1/Controllers/EstrenosProximosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PracticaSemanal1.Controllers
+{
+    public class EstrenosProximosController : Controller
+    {
+        // GET: EstrenosProximos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
